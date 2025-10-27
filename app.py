7cb956e0c39371c8f971e785b1ab8412eabd613e
Aktham Momani <aktham.momani81@gmail.com>
2025-10-27 00:47:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ div[data-testid="stFileUploader"] section[data-testid="stFileUploaderDropzone"] 
   border-radius: 12px;
 
   padding: 16px 12px;
-  min-height: 68px;
+  min-height: 66px;
   color: #6b7280;
   box-sizing: border-box;
 
